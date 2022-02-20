2e831d426064e0a589838782d7925d7081b76452
xiaoliangg1 <52221499+xiaoliangg1@users.noreply.github.com>
2022-02-20 01:31:51 -0800

    Update capulet_engine.py

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -4,15 +4,11 @@ from engine import engine
 
 
 class CapuletEngine(engine, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage, tiers):
+    def __init__(self, last_service_date, current_mileage, last_service_mileage):
         super().__init__(last_service_date)
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
-        self.tiers = tires
 
     def engine_should_be_serviced(self):
-        count = 0
-        for i in self.tires:
-            if i >= 0.9:
-                count += 1
-        return self.current_mileage - self.last_service_mileage > 30000 and count >= 1
+        return self.current_mileage - self.last_service_mileage > 30000
+
