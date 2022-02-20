7ab2318de12bff32d95997f6e2ef640d30a85cbc
xiaoliangg1 <52221499+xiaoliangg1@users.noreply.github.com>
2022-02-20 01:34:40 -0800

    Update nubbinBattery.py

--- battery/nubbinBattery.py
+++ battery/nubbinBattery.py
@@ -6,5 +6,5 @@ class NubbinBattery(battery, ABC):
     def __init__(self, last_service_date, current_date):
         super().__init__(last_service_date, current_date)
 
-    def engine_should_be_serviced(self):
+    def battery_should_be_serviced(self):
         return self.current_date - self.last_service_date > 6
