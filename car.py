be00a196c806abb96c5341b7f66c9d2aaeb429ef
xiaoliangg1 <52221499+xiaoliangg1@users.noreply.github.com>
2022-02-19 19:23:58 -0800

    Update car.py

--- car.py
+++ car.py
@@ -4,8 +4,9 @@ import serviceable
 
 
 class Car(serviceable, ABC):
-    def __init__(self, last_service_date):
+    def __init__(self, last_service_date, tires):
         super().__init__(last_service_date)
+        self.tires = tires
 
     @abstractmethod
     def needs_service(self):
