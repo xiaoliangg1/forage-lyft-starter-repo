50bc5c08a41c383513b5f5090e148e61e3ad2f49
xiaoliangg1 <52221499+xiaoliangg1@users.noreply.github.com>
2022-02-19 19:24:51 -0800

    Add files via upload

---
+++ engine/sternman_engine.py
@@ -0,0 +1,15 @@
+from abc import ABC
+
+from engine import engine
+
+
+class SternmanEngine(engine, ABC):
+    def __init__(self, last_service_date, warning_light_is_on):
+        super().__init__(last_service_date)
+        self.warning_light_is_on = warning_light_is_on
+
+    def engine_should_be_serviced(self):
+        if self.warning_light_is_on:
+            return True
+        else:
+            return False
